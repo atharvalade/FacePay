9b30dd2503aa458cb0859fbcce25410f74514c47
Atharva Lade <ladeatharva@gmail.com>
2025-06-22 21:24:00 -0400

    initial setup and contracts added

---
+++ FacePay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FacePay
+//
+//  Created by Atharva Lade on 6/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacePay/FacePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacePayApp.swift
+//  FacePay
+//
+//  Created by Atharva Lade on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FacePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
